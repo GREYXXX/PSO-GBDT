b8997809987d065989e4da39ab84ad8bff39b3a1
GREYXXX <1317357683@qq.com>
2022-09-10 23:20:34 +0800

    feat: add

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-## CITS4002 Research Project : Gradient Boosting Decision Tree Training with Swarm Intelligence
+<!-- ## CITS4002 Research Project : Gradient Boosting Decision Tree Training with Swarm Intelligence -->
 
-### Project Goal
+<!-- ### Project Goal
 Developing a robust framework to integrate the PSO algorithm to train the GBDT model.
 
 Conducting experiments to compare the predictive performance with the state-of-art GBDT implementations.
-
-
 
 ### Packages Required
 
@@ -40,6 +38,18 @@ Run PSGBDT classification by
 
 ``` python test_pretrain.py ```
 
-To run the PSGBDT for regression task, you would need to comment line 223 and uncomment line 225 and 226, since I do not have enough time to implement the python argparse.
+To run the PSGBDT for regression task, you would need to comment line 223 and uncomment line 225 and 226, since I do not have enough time to implement the python argparse. -->
+
+## Gradient Boosting Decision Tree Training using PSO
+
+### TODO
+
+1. Modify data.py. Re-organise encoding and data preprocessing.
+2. Make PSO input as (X : pd.DataFrame/np.array, y : pd.DataFrame/np.array)
+3. Finish PSO runner, make training more scalable and reusable.
+4. Add gbdt training with normal decision tree. (Attempt bitvector for decision tree base class ? To speed up DFS) 
+5. Add pso training for single decision tree and random forest.
+6. Experiment for GBDT (ODT, DT) , RF (ODT?, DT), DT. 
+
 
 
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 
+from ast import arg, main
 from data import DataSet
 from loss import SquaresError, BinomialDeviance, MultinomialDeviance
 from GBDT import GBDTRegressor, GBDTBinaryClassifier, GBDTMultiClassifier
@@ -20,142 +21,172 @@ from sklearn.metrics import accuracy_score,classification_report
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
 
 import warnings
 warnings.filterwarnings("ignore")
 
-class BasePSORunner(object):
+# iterations = 20, 
+#     size_population = 50, 
+#     max_tree_nums=6, 
+#     learning_rate = 1, 
+#     max_tree_depth = 5, 
+#     model_type = 'binary_cf',
+#     alpha = 0.45,
+#     beta = 0.45,
+#     is_bin = False
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset-path', type=str, default=os.getenv('DATASET_PATH', 'data/BankNote.csv'))
+    parser.add_argument('--model_type', type=str, default=os.getenv('MODEL_TYPE', 'binary_cf'))
+    parser.add_argument('--model_dir', type=str, default=os.getenv('MODEL_DIR', 'models/'))
+    parser.add_argument('--iterations', type=int, default=os.getenv('PSO_ITERATIONS', 20))
+    parser.add_argument('--learning_rate', type=int, default=os.getenv('LEARNING_RATE', 1))
+    parser.add_argument('--max_tree_nums', type=int, default=os.getenv('MAX_TREE_NUM', 6))
+    parser.add_argument('--max_tree_depth', type=int, default=os.getenv('MAX_TREE_DEPTH', 5))
+    parser.add_argument('--use-bin', type=bool, default=os.getenv('IF_USE_BIN', False))
 
-    def __init__(self, file, target_name, is_bin = False, standardize=False):
-        self.df = pd.read_csv(file)
-        self.df = self.df.sample(frac=1).reset_index(drop=True)
-        self.train = self.df[:int(self.df.shape[0] * 0.8)]
-        self.test  = self.df[int(self.df.shape[0] * 0.8):]
-        self.is_bin = is_bin
+    # parser.add_argument('--alpha', type=float, default=os.getenv('ALPHA_FOR_PSO', 0.45))
+    # parser.add_argument('--beta', type=float, default=os.getenv('BETA_FOR_PSO', 0.45))
 
-        is_bin = False
-        dataset = DataSet(self.train, self.test, target_name, standardize=standardize, is_bin=is_bin)
+    args, _ = parser.parse_known_args()
 
-        print("encode start...")
-        dataset.encode_table()
-        print("encode done...")
-        self.lookup_tables = dataset.get_lookup_table()
+    print(args.model_dir, args.learning_rate)
+
+# class BasePSORunner(object):
+
+#     def __init__(self, file, target_name, is_bin = False, standardize=False):
+#         self.df = pd.read_csv(file)
+#         self.df = self.df.sample(frac=1).reset_index(drop=True)
+#         self.train = self.df[:int(self.df.shape[0] * 0.8)]
+#         self.test  = self.df[int(self.df.shape[0] * 0.8):]
+#         self.is_bin = is_bin
+
+#         is_bin = False
+#         dataset = DataSet(self.train, self.test, target_name, standardize=standardize, is_bin=is_bin)
+
+#         print("encode start...")
+#         dataset.encode_table()
+#         print("encode done...")
+#         self.lookup_tables = dataset.get_lookup_table()
     
-    @property
-    def get_lookup_tables(self):
-        return self.lookup_tables
+#     @property
+#     def lookup_tables(self):
+#         return self.lookup_tables
     
-    @staticmethod
-    def plot_pso(x, y, name):
-        plt.grid(which='major', axis='y', color='darkgray')
-        plt.grid(which='major', axis='x', color='darkgray')
-        plt.plot(x,y,color='fuchsia', marker='P', linestyle='dashed')
-        plt.xlabel('Number of iteration',size = 15)
-        plt.ylabel('Fitness value',size = 15)
-        plt.title('PSO with pure search')
-        # plt.show() 
-        plt.savefig(name)
+#     @staticmethod
+#     def plot_pso(x, y, name):
+#         plt.grid(which='major', axis='y', color='darkgray')
+#         plt.grid(which='major', axis='x', color='darkgray')
+#         plt.plot(x,y,color='fuchsia', marker='P', linestyle='dashed')
+#         plt.xlabel('Number of iteration',size = 15)
+#         plt.ylabel('Fitness value',size = 15)
+#         plt.title('PSO with pure search')
+#         # plt.show() 
+#         plt.savefig(name)
 
 
-class PSOBinaryClassifierRunner(BasePSORunner):
+# class PSOBinaryClassifierRunner(BasePSORunner):
     
-    def __init__(self, file, target_name, is_bin=False, standardize=False):
-        super().__init__(file, target_name, is_bin, standardize)
+#     def __init__(self, file, target_name, is_bin=False, standardize=False):
+#         super().__init__(file, target_name, is_bin, standardize)
 
-    def run():
-        pass
+#     def run():
+#         pass
 
-class PSORegressorRunner(BasePSORunner):
+# class PSORegressorRunner(BasePSORunner):
 
-    def __init__(self, file, target_name, is_bin=False, standardize=False):
-        super().__init__(file, target_name, is_bin, standardize)
+#     def __init__(self, file, target_name, is_bin=False, standardize=False):
+#         super().__init__(file, target_name, is_bin, standardize)
 
-    def run():
-        pass
+#     def run():
+#         pass
 
 
-def pso_run(
-    name, 
-    dataset, 
-    iterations = 20, 
-    size_population = 50, 
-    max_tree_nums=6, 
-    learning_rate = 1, 
-    max_tree_depth = 5, 
-    model_type = 'binary_cf',
-    alpha = 0.45,
-    beta = 0.45,
-    is_bin = False
-    ):
+# def pso_run(
+#     name, 
+#     dataset, 
+#     iterations = 20, 
+#     size_population = 50, 
+#     max_tree_nums=6, 
+#     learning_rate = 1, 
+#     max_tree_depth = 5, 
+#     model_type = 'binary_cf',
+#     alpha = 0.45,
+#     beta = 0.45,
+#     is_bin = False
+#     ):
 
-    start = time.time()
-    pso = PSO(
-        dataset, 
-        iterations=iterations, 
-        size_population=size_population,  
-        max_tree_nums=max_tree_nums, 
-        learning_rate = learning_rate, 
-        max_tree_depth = max_tree_depth, 
-        model_type = model_type, 
-        beta = beta, 
-        alfa = alpha
-    )
-    pso.run() # runs the PSO algorithm
-    print('gbest: %s | cost: %f\n' % (pso.get_gbest().get_pbest(), pso.get_gbest().get_cost_pbest()))
-    end = time.time()
+#     start = time.time()
+#     pso = PSO(
+#         dataset, 
+#         iterations=iterations, 
+#         size_population=size_population,  
+#         max_tree_nums=max_tree_nums, 
+#         learning_rate = learning_rate, 
+#         max_tree_depth = max_tree_depth, 
+#         model_type = model_type, 
+#         beta = beta, 
+#         alfa = alpha
+#     )
+#     pso.run() # runs the PSO algorithm
+#     print('gbest: %s | cost: %f\n' % (pso.get_gbest().get_pbest(), pso.get_gbest().get_cost_pbest()))
+#     end = time.time()
 
-    if model_type == 'binary_cf':
+#     if model_type == 'binary_cf':
 
-        # Build the gbdt binary classifier with gbest solution
-        gbdt = GBDTBinaryClassifier(
-            learning_rate=learning_rate, 
-            max_depth=max_tree_depth, 
-            max_tree_nums=max_tree_nums, 
-            loss = BinomialDeviance()
-        )
-    elif model_type == 'regression':
+#         # Build the gbdt binary classifier with gbest solution
+#         gbdt = GBDTBinaryClassifier(
+#             learning_rate=learning_rate, 
+#             max_depth=max_tree_depth, 
+#             max_tree_nums=max_tree_nums, 
+#             loss = BinomialDeviance()
+#         )
+#     elif model_type == 'regression':
 
-        # Build the gbdt regressor with gbest solution
-        gbdt = GBDTRegressor(
-            learning_rate=learning_rate, 
-            max_depth=max_tree_depth, 
-            max_tree_nums=max_tree_nums, 
-            loss = SquaresError()
-        )
+#         # Build the gbdt regressor with gbest solution
+#         gbdt = GBDTRegressor(
+#             learning_rate=learning_rate, 
+#             max_depth=max_tree_depth, 
+#             max_tree_nums=max_tree_nums, 
+#             loss = SquaresError()
+#         )
     
-    elif model_type == 'multi_cf':
+#     elif model_type == 'multi_cf':
 
-        # Build the gbdt regressor with gbest solution
-        gbdt = GBDTMultiClassifier(
-            learning_rate=learning_rate, 
-            max_depth=max_tree_depth, 
-            max_tree_nums=max_tree_nums, 
-            loss = MultinomialDeviance()
-        )
-    else:
-        raise ValueError("Please input a valid model type (binary_cf, regression or multi_cf)")
+#         # Build the gbdt regressor with gbest solution
+#         gbdt = GBDTMultiClassifier(
+#             learning_rate=learning_rate, 
+#             max_depth=max_tree_depth, 
+#             max_tree_nums=max_tree_nums, 
+#             loss = MultinomialDeviance()
+#         )
+#     else:
+#         raise ValueError("Please input a valid model type (binary_cf, regression or multi_cf)")
 
-    gbdt.build_gbdt(dataset, pso.get_gbest().get_pbest())
+#     gbdt.build_gbdt(dataset, pso.get_gbest().get_pbest())
 
-    # Predict the results for test dataset
-    predict = gbdt.predict(dataset.get_test_data())
+#     # Predict the results for test dataset
+#     predict = gbdt.predict(dataset.get_test_data())
 
-    # print the accuracy
-    print(sum(predict['label'] == predict['predict_label']) / len(predict))
-    print(f"time taken is {end - start}")
+#     # print the accuracy
+#     print(sum(predict['label'] == predict['predict_label']) / len(predict))
+#     print(f"time taken is {end - start}")
 
-    y = pso.get_gbest_records()
-    x = np.arange(iterations)
+#     y = pso.get_gbest_records()
+#     x = np.arange(iterations)
 
-    base = 'pso-search'
-    if is_bin:
-        name = base + '-' + name + '-bins' + '-p' + str(size_population) + '-i' + str(iterations) 
-    else:
-        name = base + '-' + name + '-p' + str(size_population) + '-i' + str(iterations) 
-    print(name)
+#     base = 'pso-search'
+#     if is_bin:
+#         name = base + '-' + name + '-bins' + '-p' + str(size_population) + '-i' + str(iterations) 
+#     else:
+#         name = base + '-' + name + '-p' + str(size_population) + '-i' + str(iterations) 
+#     print(name)
 
-    pickle.dump((pso.get_gbest().get_pbest()), open('train_models/' + name + '.pkl', "wb"))
-    plot_pso(x, y, 'images/' + name + '.svg')
+#     pickle.dump((pso.get_gbest().get_pbest()), open('train_models/' + name + '.pkl', "wb"))
+#     plot_pso(x, y, 'images/' + name + '.svg')
+
+
 
 
 
